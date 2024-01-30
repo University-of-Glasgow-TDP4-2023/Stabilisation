703747fafc7c120e86d6291783fa9f44e524aeed
swpinkerton <swpinkerton11@gmail.com>
2024-01-30 16:40:31 +0000

    variable direction

--- testOutput/pwmtest/pwmcontrol.c
+++ testOutput/pwmtest/pwmcontrol.c
@@ -8,6 +8,7 @@
 #define out2 7
 #define enA 8
 #define pot 26
+#define bounds 0.1
 
 
 // Defining motor pins
@@ -28,13 +29,13 @@ void setup(){
     gpio_put(enA, 1);
 }
 
-void turn_right(){
+void turn_right(float a){
     gpio_put(out1,0);
-    gpio_put(out2,1);
+    gpio_put(out2,a);
 }
 
-void turn_left(){
-    gpio_put(out1,1);
+void turn_left(float a){
+    gpio_put(out1,a);
     gpio_put(out2,0);
 }
 
@@ -51,8 +52,22 @@ int main(){
     while(1){
         // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
          const float conversion_factor = 3.3f / (1 << 12);
+         const float normalised_conversion_factor = conversion_factor / 3.3f ;
          uint16_t result = adc_read();
-         printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
+         float result_norm = ((float)result * 2 * normalised_conversion_factor) - 1;
+         printf("result -1 to 1 : %f \n", result_norm);
+        //  printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor); // result in voltage mode
+         if (result_norm <= bounds && result_norm >= (- bounds)) {
+            stop();
+            printf("stop\n\r");
+         }else if (result_norm < - bounds){
+            turn_left(result * -1);
+            printf("Turn left\n\r");
+         }else if (result_norm > bounds){
+            turn_right(result);
+            printf("Turn right\n\r");
+         }
+
          sleep_ms(delay);
 
         // turn_right();
