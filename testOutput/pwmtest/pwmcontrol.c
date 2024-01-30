8c6e83acda250c8103c79f525e0b9a56f89b37a0
swpinkerton <swpinkerton11@gmail.com>
2024-01-30 18:58:38 +0000

    trying a quick pwm setup

--- testOutput/pwmtest/pwmcontrol.c
+++ testOutput/pwmtest/pwmcontrol.c
@@ -1,7 +1,8 @@
  #include <stdio.h>
  #include "pico/stdlib.h"
  #include "hardware/adc.h"
-
+ #include "hardware/pwm.h"
+// https://github.com/raspberrypi/pico-examples/blob/master/pwm/measure_duty_cycle/measure_duty_cycle.c
 
 #define delay 1000
 #define out1 6
@@ -16,29 +17,33 @@ void setup(){
     adc_init(); // for the pot input
     gpio_init(out1);
     gpio_init(out2);
-    gpio_init(enA);
+    gpio_set_function(enA, GPIO_FUNC_PWM);
     adc_gpio_init(pot);
 
     gpio_set_dir(out1, GPIO_OUT);
     gpio_set_dir(out2, GPIO_OUT);
     gpio_set_dir(enA, GPIO_OUT);
+    uint slice_num = pwm_gpio_to_slice_num(enA);
     adc_select_input(0);
     
+    pwm_config config = pwm_get_default_config();
+    pwm_init(slice_num, &config, true);
+
     gpio_put(out1,0);
     gpio_put(out2,0);
-    gpio_put(enA, 1);
+    pwm_set_gpio_level(enA, 1);
 }
 
 void turn_right(float a){
     gpio_put(out1,0);
     gpio_put(out2,1);
-    gpio_put(enA, a); // this needs to be a pwm
+    pwm_set_gpio_level(enA, a);
 }
 
 void turn_left(float a){
     gpio_put(out1,1);
     gpio_put(out2,0);
-    gpio_put(enA, a); // this needs to be a pwm
+    pwm_set_gpio_level(enA, a);
 }
 
 void stop(){
