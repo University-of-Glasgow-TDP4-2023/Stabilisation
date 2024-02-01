0ee0d4f8ff55e211c4f4d55d0a7a5de9ea00ed0a
swpinkerton <swpinkerton11@gmail.com>
2024-02-01 12:51:24 +0000

    small refactor

--- testOutput/pwmtest/pwmcontrol.c
+++ testOutput/pwmtest/pwmcontrol.c
@@ -12,6 +12,9 @@
 #define bounds 0.05
 #define wrap 1000
 
+const float conversion_factor = 3.3f / (1 << 12);
+const float normalised_conversion_factor = conversion_factor / 3.3f ;
+
 
 // Defining motor pins
 void setup(){
@@ -53,39 +56,33 @@ void stop(){
     pwm_set_gpio_level(enA, 0);
 }
 
+void motor_drive(uint16_t result){
+    float result_norm = ((float)result * 2 * normalised_conversion_factor) - 1;
+    printf("result -1 to 1 : %f \n", result_norm);
+    float power = (result_norm * (wrap+1)); // gets -1 later
+    printf("power %f: ", power);
+    if (result_norm <= bounds && result_norm >= (- bounds)) {
+        stop();
+        printf("stop\n\r");
+    }else if (result_norm < - bounds){
+        turn_left(power);
+        printf("Turn left\n\r");
+    }else if (result_norm > bounds){
+        turn_right(power);
+        printf("Turn right\n\r");
+    }
+}
+
 
 int main(){
     stdio_init_all();
     setup();
     
     while(1){
-        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-         const float conversion_factor = 3.3f / (1 << 12);
-         const float normalised_conversion_factor = conversion_factor / 3.3f ;
          uint16_t result = adc_read();
-         float result_norm = ((float)result * 2 * normalised_conversion_factor) - 1;
-         printf("result -1 to 1 : %f \n", result_norm);
-         float power = (result_norm * (wrap+1)); // gets -1 later
-         printf("power %f: ", power);
-        //  printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor); // result in voltage mode
-         if (result_norm <= bounds && result_norm >= (- bounds)) {
-            stop();
-            printf("stop\n\r");
-         }else if (result_norm < - bounds){
-            turn_left(power);
-            printf("Turn left\n\r");
-         }else if (result_norm > bounds){
-            turn_right(power);
-            printf("Turn right\n\r");
-         }
-
+         // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+         //  printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor); // result in voltage mode
+         motor_drive(result);
          sleep_ms(delay);
-
-        // turn_right();
-        // printf("Turn right\n\r");
-        // sleep_ms(1000);
-        // stop();
-        // printf("stop\n\r");
-        // sleep_ms(1000);
     }
 }
